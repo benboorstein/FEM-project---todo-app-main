44e77fadad376f9b35ea460442fae23e66f863ef
Ben <benboorstein@gmail.com>
2021-04-16 16:20:21 -0400

    js changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,21 +11,31 @@
 // 2. user types item description
     // no code needed
 
-// 3. when user hits Enter...
-// VERSION 1:
-function addListItem() {
+// 3. when user hits Enter, add what has been typed (the input value) to the list...and then remove
+function addAndRemoveItems() {
     document.getElementById('create-todo').addEventListener('keypress', function(event) {
         if (event.key == 'Enter') {
-            // ...add what has been typed (the input value) to the list
             let input = document.getElementById('create-todo') // getting the input
             let inputValue = input.value
 
             if (inputValue !== '') {
-                let list = document.getElementById('list') // getting the list
-                let newListItem = document.createElement('li'); // creating a <li> node
-                let textNode = document.createTextNode(inputValue); // converting inputValue to a text node
-                newListItem.appendChild(textNode); // appending the text node to the new list item
-                list.appendChild(newListItem); // appending the new list item to the list
+                // The new list item
+                let list = document.getElementById('list')
+                let newListItem = document.createElement('li')
+                let textNode = document.createTextNode(inputValue) // convert inputValue to a text node
+                newListItem.appendChild(textNode)
+                let refEl = document.getElementById("follows-new-items") // get the reference element (which comes AFTER the inserted element)
+                list.insertBefore(newListItem, refEl) // insert the newListItem into the list before refEl
+
+                // The new close button of the new list item
+                let closeBtn = document.createElement('span') // create a <span> node
+                let txtNd = document.createTextNode('x') // convert x to a text node
+                closeBtn.appendChild(txtNd) // append the text node to the <span>
+                closeBtn.className = 'close'
+                newListItem.appendChild(closeBtn) // append the close button to the new list item
+                closeBtn.addEventListener('click', function() { // remove list item
+                    newListItem.remove()
+                })
 
                 input.value = ''
             }   
@@ -33,9 +43,25 @@ function addListItem() {
     })
 }
 
-addListItem()
+addAndRemoveItems()
 
-// VERSION 2:
+
+// 4. "items left" number goes up with each item added and down with each item removed
+
+// 5. deal with rest at a later time
+
+
+
+
+
+
+
+
+
+
+
+
+// DOESN'T WORK
 // let input = document.getElementById('create-todo')
 // let inputValue = input.value
 
@@ -50,30 +76,13 @@ addListItem()
 //             // ...add what has been typed (the input value) to the list    
 //             if (todos[i] !== '') {
 //                 let list = document.getElementById('list') // getting the list
-//                 let newListItem = document.createElement('li'); // creating a <li> node
-//                 let textNode = document.createTextNode(todos[i]); // converting inputValue to a text node
-//                 newListItem.appendChild(textNode); // appending the text node to the new list item
-//                 list.appendChild(newListItem); // appending the new list item to the list
+//                 let newListItem = document.createElement('li') // creating a <li> node
+//                 let textNode = document.createTextNode(todos[i]) // converting inputValue to a text node
+//                 newListItem.appendChild(textNode) // appending the text node to the new list item
+//                 list.appendChild(newListItem) // appending the new list item to the list
 
 //                 input.value = ''
 //             }   
 //         }
 //     }
 // }
-
-
-
-
-
-
-
-
-
-
-// function deleteListItem() {
-
-// }
-
-// 4. "items left" number goes to 1
-
-// 5. deal with rest at a later time
