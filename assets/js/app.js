67c3fa845bd2150772428839903a98de75ff78f9
Ben <benboorstein@gmail.com>
2021-05-21 12:09:01 -0400

    js changes...active button

--- assets/js/app.js
+++ assets/js/app.js
@@ -85,35 +85,32 @@ function manageListItem(event) {
 
 
 
-// Start this over
-// Generally, MAKE SMALL FUNCTIONS, each one just doing one or a couple things, and then pass these functions into a main function.
-// Remember WHEN IN TIME things are stored (e.g., the problem accessing newListItem)
-
-// VERSION 3 (work in progress...)
+/* VERSION 3 (this version works well, but Version 4 is different insofar as how list items are presented with the All, Active, and Completed buttons)
 
 // Q: Why is hitting Enter working with the program the way it is?
 // A: The Enter key still works probably because pressing Enter within any form element triggers the submission of the form...equivalent to clicking the button. At least I think. Regardless, it works.
 
 let input = document.getElementById('create-todo') // get the input
 const list = document.getElementById('list')
-let numItemsLeft = document.getElementById('num-items-left') // NEW LINE - self-explanatory
-numItemsLeft.textContent = 0 // NEW LINE - sets 'items left' number to 0 upon loading
+let numItemsLeft = document.getElementById('num-items-left')
+numItemsLeft.textContent = 0 // sets 'items left' number to 0 upon loading
 
 document.getElementById('add-item-btn').addEventListener('click', function(event) {
 
     if (input.value !== '') {
         event.preventDefault() // this is saying: don't refresh the page each time a new item is submitted
 
-        addTodo()
+        addToDo()
     }
 })
 
-// addTodo creates an 'li', sets its text, and adds it to the list
-function addTodo() {
+// addToDo creates an 'li', sets its text, and adds it to the list
+function addToDo() {
     // list item anatomy
     let newListItem = document.createElement('li')
     newListItem.className = 'new-list-item is-hidden' // may not need to keep the 'new-list-item' class
     newListItem.id = toDos.length + 1 // Remember this matches what's getting pushed to the toDos array just below
+    newListItem.draggable = true // KEEP??
     let textNode = document.createTextNode(input.value) // convert input.value to a text node
     let inputTextSpan = document.createElement('span') // create a <span> node
     inputTextSpan.className = 'item-completed'
@@ -126,8 +123,6 @@ function addTodo() {
 
     inputTextSpan.classList.remove('item-completed') // this needs to be here because of inputTextSpan's 'item-completed' class being set (in the CSS) to 'text-decoration: line-through;'
     newListItem.classList.remove('is-hidden') // this needs to be here because of newListItem's 'is-hidden' class being set (in the CSS) to 'display: none;'
-    // newListItem.style.display = 'block' // works but not what I want
-    // document.getElementsByClassName('new-list-item').classList.remove('is-hidden') // doesn't work and I'm not sure why
 
     toDos.push(
         {
@@ -137,12 +132,12 @@ function addTodo() {
         }
     )
 
-    // addTodo also invokes two other functions, 'addCloseBtn' and 'prepareUI'
-    addCloseBtn(newListItem) // addCloseBtn accepts one argument - the newly created list item in addTodo. ***This is key so that the addCloseBtn function has access to newListItem.***
+    // addToDo also invokes two other functions, 'addCloseBtn' and 'prepareUI'
+    addCloseBtn(newListItem) // addCloseBtn accepts one argument - the newly created list item in addToDo. ***This is key so that the addCloseBtn function has access to newListItem.***
     addCheckbox(newListItem, inputTextSpan)
     prepareUI()
-
-    numItemsLeft.textContent = toDos.length // NEW LINE - increments the 'items left' number every time a list item is added 
+        
+    updateItemsLeft(newListItem, false)
 }
 
 // addCloseBtn creates the 'x' span, appends it to the list item, and sets up its event listener (to remove the corresponding list item when clicked)
@@ -157,9 +152,8 @@ function addCloseBtn(newItem) {
     newItem.appendChild(closeBtn) // append the close button to the new list item
 
     closeBtn.addEventListener('click', function(event) {
-        event.target.parentElement.remove() // this just removes the list item from the UI
-        toDos.splice(toDos[newItem.id - 1], 1) // NEW LINE - removes from the toDos array the item on which the 'x' is clicked - QQQ: this seems to work, but see in console how the IDs reorder...Is this what we want?
-        numItemsLeft.textContent = toDos.length // NEW LINE - resets the 'items-left' number, given the change in the toDos array length
+        event.target.parentElement.remove()
+        updateItemsLeft(newItem, true)
     })
 }
 
@@ -171,11 +165,10 @@ function addCheckbox(newItem, textSpan) {
     checkbox.addEventListener('click', function(event) { // better to have event be 'click' or 'change' here? Seems to make no difference
         if (event.target.checked) {
             textSpan.classList.add('item-completed')
-            toDos.splice(toDos[newItem.id - 1], 1) // NEW LINE - QQQ: this works here, but because it needs to be able to be undone, I'm not sure it's the correct way to do it in this particular case 
-            numItemsLeft.textContent = toDos.length // NEW LINE - QQQ: same as line just above
+            updateItemsLeft(newItem, true)
         } else {
             textSpan.classList.remove('item-completed')
-            // QQQ: Assuming the user has checked the box (and therefore the 'items left' number goes down 1) but then decides to uncheck the box (and therefore the 'items left' number needs to go back up 1, i.e., the previous action from the 'if' clause needs to be UNDONE), where/how do I handle this?
+            updateItemsLeft(newItem, false) 
         }
     })
 }
@@ -188,12 +181,17 @@ function prepareUI() {
 
 let toDos = []
 
+function updateItemsLeft(item, bool) {
+    toDos[item.id - 1].completed = bool // false means increasing 'items left' number, true means decreasing 'items left' number
+    let newToDos = toDos.filter(toDosObj => toDosObj.completed == false)
+    numItemsLeft.textContent = newToDos.length
+}
+
 // "Clear Completed" button - seems to be working properly
 document.getElementById('clear-completed-btn').addEventListener('click', function() {
     Array.from(list.children).forEach(listItem => { // 'list' (which is the variable name for my 'ul') is just one object, so we need to use 'list.children'. 'list.children', however, is an array-like object, not an array, so we need to make it an array with 'Array.from'
         if (listItem.firstElementChild.checked) { // 'firstChild' or 'firstElementChild'? Doesn't seem to make a difference here
             listItem.remove()
-            toDos.splice(toDos[listItem.id - 1], 1) // NEW LINE - seems to work properly here
         }
     })
 })
@@ -231,8 +229,247 @@ document.getElementById('completed-btn').addEventListener('click', function() {
     })
 })
 
-/* LEFT OFF HERE
-OoO:
-- updating "items left"
-- drag and drop
+
+//'drag and drop to reorder list' - what I have doesn't really work - table this for now
+// might help: https://www.javascripttutorial.net/web-apis/javascript-drag-and-drop/
+
+// Array.from(list.children).forEach(listItem => {
+//     listItem.addEventListener('dragstart', function(event) { // note: I added the 'draggable' attribute to newListItem at the creation of it
+//         event.preventDefault
+        
+//     })
+
+//     listItem.addEventListener('drop', function(event) {
+//         event.preventDefault
+//         listItem.draggable = false
+//         document.getElementById('drop-under-here').appendChild(listItem)
+//     })
+// })
+
 */
+
+
+
+
+
+
+
+
+
+
+
+// VERSION 4 (work in progress...)
+
+// Q: Why is hitting Enter working with the program the way it is?
+// A: The Enter key still works probably because pressing Enter within any form element triggers the submission of the form...equivalent to clicking the button. At least I think. Regardless, it works.
+
+let input = document.getElementById('create-todo') // get the input
+const list = document.getElementById('list')
+let numItemsLeft = document.getElementById('num-items-left')
+numItemsLeft.textContent = 0 // sets 'items left' number to 0 upon loading
+
+document.getElementById('add-item-btn').addEventListener('click', function(event) {
+
+    if (input.value !== '') {
+        event.preventDefault() // this is saying: don't refresh the page each time a new item is submitted
+
+        addToDo()
+    }
+})
+
+// addToDo creates an 'li', sets its text, and adds it to the list
+function addToDo() {
+    // list item anatomy
+    let newListItem = document.createElement('li')
+    newListItem.className = 'new-list-item' // may not need to keep the 'new-list-item' class
+    newListItem.id = toDos.length + 1 // Remember this matches what's getting pushed to the toDos array just below
+    // newListItem.draggable = true // KEEP??
+    let textNode = document.createTextNode(input.value) // convert input.value to a text node
+    let inputTextSpan = document.createElement('span') // create a <span> node
+    inputTextSpan.className = 'item-completed'
+    inputTextSpan.appendChild(textNode)
+    newListItem.appendChild(inputTextSpan)
+
+    // place the list item
+    let refEl = document.getElementById("follows-new-items") // get the reference element (which comes AFTER the inserted element)
+    list.insertBefore(newListItem, refEl) // insert the newListItem into the list before refEl
+
+    inputTextSpan.classList.remove('item-completed') // this needs to be here because of inputTextSpan's 'item-completed' class being set (in the CSS) to 'text-decoration: line-through;'
+
+    toDos.push(
+        {
+            text: input.value,
+            completed: false,
+            id: toDos.length + 1
+        }
+    )
+
+    // addToDo also invokes two other functions, 'addCloseBtn' and 'prepareUI'
+    addCloseBtn(newListItem) // addCloseBtn accepts one argument - the newly created list item in addToDo. ***This is key so that the addCloseBtn function has access to newListItem.***
+    addCheckbox(newListItem, inputTextSpan)
+    prepareUI()
+        
+    updateItemsLeft(newListItem, false)
+}
+
+// addCloseBtn creates the 'x' span, appends it to the list item, and sets up its event listener (to remove the corresponding list item when clicked)
+function addCloseBtn(newItem) {
+    // The new close button (of the new list item) anatomy
+    let closeBtn = document.createElement('span') // create a <span> node
+    closeBtn.className = 'close'
+    let txtNd = document.createTextNode('x') // convert x to a text node
+
+    // place the new close button
+    closeBtn.appendChild(txtNd) // append the text node to the <span>
+    newItem.appendChild(closeBtn) // append the close button to the new list item
+
+    closeBtn.addEventListener('click', function(event) {
+        event.target.parentElement.remove()
+        updateItemsLeft(newItem, true)
+    })
+}
+
+function addCheckbox(newItem, textSpan) {
+    let checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    newItem.insertBefore(checkbox, textSpan) // insert the checkbox into the newListItem before inputTextSpan
+
+    checkbox.addEventListener('click', function(event) { // better to have event be 'click' or 'change' here? Seems to make no difference
+        if (event.target.checked) {
+            textSpan.classList.add('item-completed')
+            updateItemsLeft(newItem, true)
+        } else {
+            textSpan.classList.remove('item-completed')
+            updateItemsLeft(newItem, false) 
+        }
+    })
+}
+
+// prepareUI resets the input's value and focuses it
+function prepareUI() {
+    input.value = '' // clear input field
+    input.focus() // set cursor back to beginning of input field
+}
+
+let toDos = []
+
+function updateItemsLeft(item, bool) {
+    toDos[item.id - 1].completed = bool // false means increasing 'items left' number, true means decreasing 'items left' number
+    let newToDos = toDos.filter(toDosObj => toDosObj.completed == false)
+    numItemsLeft.textContent = newToDos.length
+}
+
+// "Clear Completed" button - seems to be working properly
+document.getElementById('clear-completed-btn').addEventListener('click', function() {
+    Array.from(list.children).forEach(listItem => { // 'list' (which is the variable name for my 'ul') is just one object, so we need to use 'list.children'. 'list.children', however, is an array-like object, not an array, so we need to make it an array with 'Array.from'
+        if (listItem.firstElementChild.checked) { // 'firstChild' or 'firstElementChild'? Doesn't seem to make a difference here
+            listItem.remove()
+        }
+    })
+})
+
+
+
+
+
+// *************************** working on the below *******************************
+
+/*
+The UL with id "list" is a whiteboard.
+Your toDos array is a database.
+
+Whenever one of the three buttons is clicked...
+- Erase the whiteboard so you start clean (the whiteboard has no memory of what was just written on it)
+- Refer to the database, containing all the items ever added to it (minus any items Cleared Completed) // QQQ: ao now CCs are in as true
+- Filter it by the ones that should be displayed based on the button that was clicked
+- And 'write' those items on the whiteboard, in the order they were added to the database
+*/
+
+// // "All" button - bad
+// document.getElementById('all-btn').addEventListener('click', function() {
+//     Array.from(list.children).forEach(listItem => {
+//         if (listItem.firstElementChild.checked || listItem.firstElementChild.checked == false) {
+//             listItem.remove()
+//         }
+//     })
+// })
+
+// "Active" button - bad
+document.getElementById('active-btn').addEventListener('click', function() {
+    Array.from(list.children).forEach(listItem => {
+        if (listItem.firstElementChild.checked || listItem.firstElementChild.checked == false) {
+            listItem.remove()
+            // numItemsLeft.textContent = 0 // NEED?
+        }
+    })
+    let newToDos = toDos.filter(toDosObj => toDosObj.completed == false)
+
+    for (let i = 0; i < newToDos.length; i++) {
+
+        newToDos[i] = document.createElement('li')
+        newToDos[i].className = 'new-list-item'
+        newToDos[i].id = toDos.length + 1
+        let textNode = document.createTextNode(input.value)
+        let inputTextSpan = document.createElement('span')
+        inputTextSpan.className = 'item-completed'
+        inputTextSpan.appendChild(textNode)
+        newToDos[i].appendChild(inputTextSpan)
+
+        let refEl = document.getElementById("follows-new-items")
+        list.insertBefore(newToDos[i], refEl)
+
+        inputTextSpan.classList.remove('item-completed')
+
+        // toDos.push(
+        //     {
+        //         text: input.value,
+        //         completed: false,
+        //         id: toDos.length + 1
+        //     }
+        // )
+
+        addCloseBtn(newToDos[i])
+        addCheckbox(newToDos[i], inputTextSpan)
+        prepareUI()
+            
+        updateItemsLeft(newToDos[i], false)
+    }
+    
+
+    // numItemsLeft.textContent = ????? // NEED?
+})
+
+// // "Completed" button - bad
+// document.getElementById('completed-btn').addEventListener('click', function() {
+//     Array.from(list.children).forEach(listItem => {
+//         if (listItem.firstElementChild.checked || listItem.firstElementChild.checked == false) {
+//             listItem.remove()
+//         }
+//     })
+// })
+
+
+
+
+
+
+
+
+
+
+
+//'drag and drop to reorder list' - what I have doesn't really work - table this for now
+// might help: https://www.javascripttutorial.net/web-apis/javascript-drag-and-drop/
+
+// Array.from(list.children).forEach(listItem => {
+//     listItem.addEventListener('dragstart', function(event) { // note: I added the 'draggable' attribute to newListItem at the creation of it
+//         event.preventDefault
+        
+//     })
+
+//     listItem.addEventListener('drop', function(event) {
+//         event.preventDefault
+//         listItem.draggable = false
+//         document.getElementById('drop-under-here').appendChild(listItem)
+//     })
+// })
